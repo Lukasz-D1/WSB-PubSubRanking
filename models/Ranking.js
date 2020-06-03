eb2e758eacfa86372269f28d9a8b578b7856399c
Lukasz Dobek <lukasz.dobek@outlook.com>
2020-06-03 12:51:14 +0200

    Add features to Ranking class

--- models/Ranking.js
+++ models/Ranking.js
@@ -11,24 +11,45 @@ class Ranking {
         this.ranker = redis.createClient();
         this.subscriber.on("message", (channel, message) => {
             console.log(`${channel} ${message}`);
-            this.saveToRanking(JSON.parse(message));
+            if (channel == "recipe_ranking") {
+                let msgObj = JSON.parse(message);
+                if (msgObj["remove"]) {
+                    this.deleteFromRanking(msgObj);
+                    
+                } else {
+                    this.saveToRanking(msgObj);
+                }
+            }
         });
     }
 
     saveToRanking(message) {
         let recipeId = message["id_recipe"];
-        this.ranker.zadd(this.channelName, "INCR", 1, recipeId, console.log);
-    }
-       
-    async getRanking(n) {
-        let result = await this.ranker.zrangeAsync(this.channelName, 0, n, "WITHSCORES");
-        return result;
+        if (message["decr"]) {
+            this.ranker.zincrby(this.channelName, -1, recipeId);
+        } else {
+            this.ranker.zadd(this.channelName, "INCR", 1, recipeId);
+        }
     }
 
-    deleteFromRanking(recipeId) {
+    deleteFromRanking(message) {
+        let recipeId = message["id_recipe"];
         this.ranker.zrem(this.channelName, recipeId);
     }
     
+    async getRecipeRanking(id) {
+        let score = await this.ranker.zscoreAsync(this.channelName, id);
+        let rank = await this.ranker.zrevrankAsync(this.channelName, id);
+        return {
+            "score": score,
+            "rank": rank+1
+        };
+    }
+
+    async getRanking(n = -1) {
+        let result = await this.ranker.zrangeAsync(this.channelName, 0, n, "WITHSCORES");
+        return result;
+    }    
 }
 
 module.exports.Ranking = Ranking;
